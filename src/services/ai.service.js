f450b0b2e4e7c0cc0ad4337289a1190061b60dd2
Rohan Kumar Sharma <your_github_email@gmail.com>
2026-01-22 16:09:06 +0530

    4th

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -6,11 +6,25 @@ const ai = new GoogleGenAI({});
 async function generateResponse(content) {
   const response = await ai.models.generateContent({
     model: "gemini-3-flash-preview",
-    contents: content
+    contents: content,
   });
-  return response.text
+  console.log(response.text);
+  return response.text;
 }
 
-module.exports = {
-    generateResponse
+
+async function generateVector(content) {
+  const response = await ai.models.embedContent({
+    model: "gemini-embedding-001",
+    contents: content,
+    config: {
+      outputDimensionality: 768,
+    },
+  });
+  return response.embeddings[0].values;
 }
+
+module.exports = { 
+  generateResponse,
+  generateVector,
+};
--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
 const aiService = require("../services/ai.service");
 const messageModel = require("../models/message.model");
+const { createMemory, queryMemory } = require("../services/vector.service");
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
@@ -30,38 +31,87 @@ function initSocketServer(httpServer) {
   });
 
   io.on("connection", (socket) => {
+
     socket.on("ai-message", async (messagePayload) => {
       console.log(messagePayload); /* (chat , content) */
+      
 
-      await messageModel.create({
+      const message = await messageModel.create({
         chat: messagePayload.chat,
         user: socket.user._id,
         content: messagePayload.content,
-        role: "user",
+        role: "user", 
       });
 
-      const chatHistory = await messageModel.find({
+      const vectors = await aiService.generateVector(messagePayload.content);
+      
+      const memory = await queryMemory({
+        queryVector: vectors,
+        limit: 3,
+        metadata: {}, 
+      });
+
+      await createMemory({
+        vectors,
+        messageId: message._id, //unique id dena hoga har message ka 
+        metadata: {
+          chat: messagePayload.chat,
+          user: socket.user._id
+        },
+      });
+
+      console.log(memory);
+
+      const chatHistory = (await messageModel.find({
         chat: messagePayload.chat
-      })
+      }).sort({ createdAt: -1 }).limit(20).lean()).reverse(); //ye 20 limit tk yaad rakhega sort lagane se 
+        
+        
+      // if (chatHistory.length == 0) {
+      //   const response = await aiService.generateResponse(
+      //     "array khali hai re developers",
+      //   );
+      // } else {
+      //   const response = await aiService.generateResponse(
+      //     chatHistory.map((item) => {
+      //       return {
+      //         role: item.role,
+      //         parts: [{ text: item.content }],
+      //       };
+      //     }),
+      //   );
+      // }
 
-      const response = await aiService.generateResponse(chatHistory.map(item => {
-        return {
-        role: item.role,
-        parts: [ {text: item.content}]
-        }
-      }));
+      const response = await aiService.generateResponse(
+        chatHistory.map((item) => {
+          return {
+            role: item.role,
+            parts: [{ text: item.content }]
+          };
+        }));
 
-      await messageModel.create({
+      const responseMessage = await messageModel.create({
         chat: messagePayload.chat,
         user: socket.user._id,
         content: response,
         role: "model",
       });
 
+      const responseVectors = await aiService.generateVector(response);
 
-      socket.emit("ai-response", { //iske through user ko response send kr deta hai ai
+      await createMemory({
+        vectors: responseVectors,
+        messageId: responseMessage._id, //unique id dena hoga har message ka
+        metadata: {
+          chat: messagePayload.chat,
+          user: socket.user._id
+        },
+      });
+
+      socket.emit("ai-response", {
+        //iske through user ko response send kr deta hai ai
         content: response,
-        chat: messagePayload.chat,
+        chat: messagePayload.chat
       });
     });
   });
